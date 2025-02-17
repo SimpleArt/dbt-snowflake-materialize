cbc8e5f1f4fa0f83a12a67534e4bfaf18bc6c7bd
SimpleArt <nguyej17@my.erau.edu>
2025-02-17 17:16:40 -0500

    Implemented branch prediction optimization
    The most common branch path for a materialization is that the object already exists and is the same materialization. Normally this would require 2 queries: 1 information schema query to get the type followed by 1 show query. This runs only the show query in the most common scenario and only runs 2 queries if the object does not exist or exists with a different type. This especially improves the built time for models which are fast to build, such as views, streams, functions, etc.

--- macros/relation_utils/drop_relation.sql
+++ macros/relation_utils/drop_relation.sql
@@ -1,8 +1,8 @@
 {% macro drop_relation(relation, drop_unless_type=none, metadata=none, transient=none) %}
 
-    {% set result = get_relation_type(relation) %}
+    {% set result = get_relation_type(relation, drop_unless_type) %}
     {% set type = result.get('type') %}
-    {% set row = result.get('row') %}
+    {% set rows = result.get('rows') %}
 
     {% if drop_unless_type is none %}
         {% set drop_unless_type = relation.type %}
@@ -15,17 +15,9 @@
         {{ return('create or replace') }}
 
     {% elif type in ['function', 'procedure'] %}
-        {% if type == 'function' %}
-            {% set show_type = 'user function' %}
-        {% else %}
-            {% set show_type = 'procedure' %}
-        {% endif %}
-
-        {% set results = run_query(show_relation(relation, show_type)) %}
-
         {# If a function/procedure needs to be created, first check if it's already created. #}
         {% if type == drop_unless_type %}
-            {% for row in results if row['name'] == relation.identifier %}
+            {% for row in rows %}
                 {% set comment = row.get('comment', row.get('description', '')) %}
                 {% set state = {'flag': true} %}
 
@@ -40,7 +32,7 @@
 
         {# If no matching function/procedure is found, drop all functions/procedures. #}
         {% else %}
-            {% for row in results if row['name'] == relation.identifier %}
+            {% for row in rows %}
                 {% call statement('drop_callable') %}
                     drop {{ type }} if exists {{ relation }}({{ row['arguments'][(relation.identifier|length + 1):].split(') RETURN ')[0] }})
                 {% endcall %}
@@ -61,22 +53,24 @@
     {% endif %}
 
     {# If the object is a stream, a "show streams" query was already ran. #}
-    {% if type == 'stream' and row is not none %}
-        {% set comment = row.get('comment', row.get('description')) %}
+    {% if type == 'stream' %}
+        {% for row in rows %}
+            {% set comment = row.get('comment', row.get('description')) %}
 
-        {% if row.get('stale') == 'true' %}
-            {{ return('create or replace') }}
-
-        {% elif metadata is not none %}
-            {% for part in metadata if part not in comment %}
+            {% if row.get('stale') == 'true' %}
                 {{ return('create or replace') }}
-            {% endfor %}
 
-        {% endif %}
+            {% elif metadata is not none %}
+                {% for part in metadata if part not in comment %}
+                    {{ return('create or replace') }}
+                {% endfor %}
+
+            {% endif %}
+        {% endfor %}
 
     {# Check if the existing object matches the expected object. #}
     {% elif metadata is not none or transient is not none %}
-        {% for row in run_query(show_relation(relation, type)) %}
+        {% for row in rows %}
             {% set text = row.get('text') %}
             {% set comment = row.get('comment', row.get('description')) %}
             {% set scheduling_state = row.get('scheduling_state') %}
